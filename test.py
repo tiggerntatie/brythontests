e6972b3a62dfe6c81a212ff67ddd84aa564debec
Eric Dennison <ericd@netdenizen.com>
2016-06-18 16:38:41 -0400

    Updated from Brython Server: 6/18/2016 4:38:41 PM

--- test.py
+++ test.py
@@ -9,3 +9,13 @@ for i in range(1000000):
 t = time.time() - t0
 
 print("one million iterations in ", t, " seconds")
+# real python3 is about 100x faster
+
+x = 0
+t0 = time.time()
+for i in range(1000000):
+    x = x+1
+    
+t = time.time() - t0
+print("one million iterations in ", t, " seconds")
+# real python3 is about 100x faster
